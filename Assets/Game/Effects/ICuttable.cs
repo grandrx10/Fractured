6e434c71cd1e076e759af6f2a543fbaae1a74b86
Alex Tian Ye Xu <44008411+fmwofmwmf@users.noreply.github.com>
2025-12-25 01:18:27 -0700

    git

--- Assets/Game/Effects/ICuttable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Cards.Card_Assets.RPS.Behaviors
-{
-    public interface ICuttable
-    {
-        public void Cut(Vector3 cutNormal, Vector3 cutPosition);
-    }
-}
